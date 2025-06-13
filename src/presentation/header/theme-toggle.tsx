09591a43724b99aba2d3c7787ba5f17fe7f6c093
suzuki3jp <suzuki3.info@gmail.com>
2025-06-13 10:47:24 +0900

    feat: add Header and theming system

---
+++ src/presentation/header/theme-toggle.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+import { Button } from "@/presentation/common/shacn/button";
+
+export function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Button variant="ghost" size="icon" className="h-9 w-9 opacity-0" />;
+  }
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      className="h-9 w-9 hover:cursor-pointer"
+      onClick={toggleTheme}
+    >
+      <Sun className="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
---
+++ src/presentation/providers/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { ThemeProvider } from "./theme-provider";
+
+/**
+ * Providers component that wraps the application with necessary providers
+ * @param param0
+ * @returns
+ */
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
