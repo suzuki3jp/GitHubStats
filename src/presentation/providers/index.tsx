5d1e67cf270d11b9e0496bdf1a4205fc56905c6e
suzuki3jp <suzuki3.info@gmail.com>
2025-06-25 19:21:19 +0900

    chore: move next-auth handler to `/next-auth`

--- src/presentation/providers/index.tsx
+++ src/presentation/providers/index.tsx
@@ -18,7 +18,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
     >
       <CookiesProvider>
-        <SessionProvider basePath="/auth">
+        <SessionProvider basePath="/next-auth">
           <AuthProvider>{children}</AuthProvider>
         </SessionProvider>
       </CookiesProvider>
